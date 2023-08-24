40baa60c245f6a6cf989c77c14eb6514e4a6b707
EmanueleBrusa <emanuelebrusa@hotmail.it>
2023-08-24 05:49:49 +0200

    popolato post db con seeder

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,7 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Support\Str;
+
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'content', 'slug'];
+
+    public static function generateslug($title)
+    {
+        return Str::slug($title, '-');
+    }
 }
